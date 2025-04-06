fe219f3397f1a164801cf34e508acf6369790b63
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-04-06 09:59:12 +0200

    fix: project name

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 def main() -> None:
     """Log simple hello message."""
-    logger.info("Hello from draftmaster-rest-api!")
+    logger.info("Hello from pactdesk-rest-api!")
 
 
 if __name__ == "__main__":
